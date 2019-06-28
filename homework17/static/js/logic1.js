bd34d24348210239becd6da06115e1520ae002fc
Pamela Zhao <pamzhao@yahoo.com>
2019-06-27 23:49:42 -0700

    Hw17

---
+++ homework17/static/js/logic1.js
@@ -0,0 +1,103 @@
+// Past 7 days earthquake data
+//var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Past day earthquake data
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+// Perform a GET request to the url
+d3.json(url, function(data) {
+  console.log(data);
+
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+  console.log(earthquakeData);
+  // Create a GeoJson layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h4>Place: " + feature.properties.place + "</h4><hr><p>Date: " + new Date(feature.properties.time) + 
+        "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+    },
+
+    pointToLayer: function(feature, latlng) {
+      return new L.Circle(latlng, {
+        radius: setRadius(feature.properties.mag),
+        fillColor: setColor(feature.properties.mag),
+        fillOpacity: 0.8,
+        color: "grey",
+        weight: 0.8
+      })
+    }
+  });
+
+  // Sending the earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+  console.log(earthquakes);
+  // Define Satellite, Greyscale, and Outdoor layers
+  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets-satellite",
+    accessToken: API_KEY
+  });
+
+  var greyscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMap object to hold the three layers
+  var baseMaps = {
+    "Satellite": satellite,
+    "Greyscale": greyscale,
+    "Outdoors": outdoors
+  };
+
+  // Create overlay object to hold the overlay layer
+  var overlayMap = {
+    "Earthquakes": earthquakes
+  };
+
+  // Create myMap, giving it the satellite map and earthquakes layers to display on load
+  let myMap = L.map('mapid', {
+    center: [37.09, -95.71],
+    zoom: 3,
+    layers: [satellite, earthquakes]
+  });
+  
+  // Create a layer control
+  // Pass in the baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMap, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
+// Return color base on value
+function setColor(x) {
+  return x > 5 ? "#FF0000" :
+        x > 4 ? "#FF6600" :
+        x > 3 ? "#FF9900" :
+        x > 2 ? "#FFCC00" :
+        x > 1 ? "#FFFF00" :
+               "#66FF00";
+}
+
+// set the circle radius
+function setRadius(x) {
+  return x*1000;
+}
